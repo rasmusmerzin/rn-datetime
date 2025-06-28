025b0375b14bf4a67d8d17e74e451961bb2047de
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-06-29 02:45:49 +0300

    fix example script working directory

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+cd "$(dirname "$(dirname "$(readlink -f "$0")")")" || exit 1
+
 if [ -f package-lock.json ]
 then npm run prepare
 else npm install
 fi
 
-cd "example/$1"
+cd example
 [ -f package-lock.json ] || npm install
 rm -rf node_modules/rn-datetime/dist
 cp -r ../dist node_modules/rn-datetime/dist
