fe0d3833b3f08a1269de83e5d09a497e9da68b90
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-06-29 07:32:38 +0300

    made web compat modal fade in and out which mimics android modal

--- src/compat.web.tsx
+++ src/compat.web.tsx
@@ -1,10 +1,24 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useMemo } from "react";
 
 interface Props {
   visible: boolean;
   children?: ReactNode;
 }
 
-export const Modal = ({ visible, children }: Props) => (
-  <div style={{ display: visible ? undefined : "none" }}>{children}</div>
-);
+export const Modal = ({ visible, children }: Props) => {
+  const style: React.CSSProperties = useMemo(
+    () => ({
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      pointerEvents: visible ? undefined : "none",
+      userSelect: visible ? "auto" : "none",
+      opacity: visible ? 1 : 0,
+      transition: "opacity 300ms",
+    }),
+    [visible],
+  );
+  return <div style={style}>{children}</div>;
+};
